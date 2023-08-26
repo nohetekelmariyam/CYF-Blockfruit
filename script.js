d91e92add65692742fcbc7cea4cf367a6f582f01
nohetekelmariyam <127329572+nohetekelmariyam@users.noreply.github.com>
2023-08-26 17:12:49 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let element_banana = document.getElementById('banana');
+element_banana.style.color = '#ffcc66';
+element_banana.style.backgroundColor = '#333300';
+let element_apple = document.getElementById('apple');
+element_apple.style.color = '#009900';
+element_apple.style.backgroundColor = '#333300';
+let element_strawberry = document.getElementById('strawberry');
+element_strawberry.style.color = '#ff0000';
+element_strawberry.style.backgroundColor = '#333300';
